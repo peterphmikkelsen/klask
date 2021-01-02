3c7daaf0d8da8522d05a25b7f317101030ddfa0d
peter mikkelsen <peterphmikkelsen@gmail.com>
2021-01-02 02:06:35 +0100

    testing app.

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val app = KFlask()
+    app.route("/") { _, res ->
+        res.renderTemplate("index.html")
+    }
+
+    app.route("/hello") { _, res ->
+        res.makeResponse("Hello World!", "text/plain")
+    }
+
+    app.route("/json") { _, res ->
+        res.makeResponse("""{"hello":"world!"}""", "application/json")
+    }
+
+    app.route("/test", methods = listOf("POST")) { req, res ->
+        println(req.body)
+        res.makeResponse("home=Cosby&favorite+flavor=flies", "application/x-www-form-urlencoded")
+    }
+
+    app.run()
+}
