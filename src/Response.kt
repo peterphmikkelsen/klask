f508b6db3d7affb7e09e7841fb5e7e2268aaaebc
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2021-01-16 00:27:46 +0100

    added Date to response header.

--- src/Response.kt
+++ src/Response.kt
@@ -1,14 +1,23 @@
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Response(var body: String = "", var contentType: String = "") {
+
+    private val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss")
+    init {
+        dateFormat.timeZone = TimeZone.getTimeZone("GMT")
+    }
 
     fun renderTemplate(fileName: String): Response {
         val file = File("test/templates/$fileName") // TODO: Fix hardcoded path
         val reader = file.bufferedReader()
         val sb = StringBuilder()
-        sb.append("HTTP/1.1 200 OK\n")
-        sb.append("Content-Type: text/html; charset=utf-8\n")
-        sb.append("Content-Length: ${file.length()}\r\n\n")
+        sb.append("HTTP/2 200 OK\n")
+            .append("Content-Type: text/html; charset=utf-8\n")
+            .append("Connection: keep-alive\n")
+            .append("Date: ${dateFormat.format(Date())} GMT\r\n\n")
+
         var line = reader.readLine()
         while (line != null) {
             sb.append("$line\n")
@@ -23,7 +32,11 @@ class Response(var body: String = "", var contentType: String = "") {
 
     fun makeResponse(response: String, contentType: Content): Response {
         val sb = StringBuilder()
-        sb.append("HTTP/1.1 200 OK\n").append("Content-Type: ${contentType.desc}; charset=utf-8\n").append("Content-Length: ${response.length}")
+        sb.append("HTTP/2 200 OK\n")
+            .append("Content-Type: ${contentType.desc}; charset=utf-8\n")
+            .append("Connection: keep-alive\n")
+            .append("Date: ${dateFormat.format(Date())} GMT\n")
+            .append("Content-Length: ${response.length}")
         if (contentType.desc == "application/json")
             sb.append("\nAccept: application/json")
         sb.append("\r\n\n$response")
