0668c662b01ddb88f031ffa7f539d32a353f00ff
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2021-01-15 18:25:18 +0100

    Users now only have to specify the content-type through a more understandable enum class.

---
+++ src/Content.kt
@@ -0,0 +1,7 @@
+enum class Content(val desc: String) {
+    HTML("text/html"),
+    XML("application/xml"),
+    PLAIN("text/plain"),
+    JSON("application/json"),
+    URLEncoded("application/x-www-form-urlencoded")
+}
