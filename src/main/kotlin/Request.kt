1e18167d806065a10308366fbb267169fa08d494
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2022-05-19 20:11:12 +0200

    added a method for directly decoding request body from JSON

--- src/main/kotlin/Request.kt
+++ src/main/kotlin/Request.kt
@@ -1,3 +1,6 @@
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
+
 class Request(var method: String = "",
               var protocol: String = "",
               var protocolVersion: String = "",
@@ -9,4 +12,15 @@ class Request(var method: String = "",
               var body: String = "",
               var params: MutableMap<String, Any> = mutableMapOf(),
               var queries: MutableMap<String, String> = mutableMapOf(),
-)
+) {
+    inline fun <reified T> receiveJsonObject(): T {
+        if (method != "POST" && method != "PUT")
+            throw IllegalArgumentException("The used method must be either POST or PUT, it was $method.")
+
+        if (contentType != Content.JSON)
+            throw Klask.JsonDecodeException("Content-Type must be ${Content.JSON.desc}, it was ${contentType.desc}.")
+
+        return Json.decodeFromString(body)
+    }
+}
+
