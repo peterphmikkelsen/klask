b19f021679ef8dad78e26476e52657223265e9a9
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2022-05-19 20:09:03 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions.useIR = true
 
 group = "me.peter"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 }
 
 repositories {
